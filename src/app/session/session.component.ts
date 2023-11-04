39c517ed72dcc54cd6a5dfd38b41ec84169af1f3
Predator Monarch <predator.corp@proton.me>
2023-11-04 14:43:19 +0000

    Update session.component.ts

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -8,37 +8,48 @@ import { client, room } from '../acccueil/acccueil.component';
   styleUrls: ['./session.component.css']
 })
 export class SessionComponent {
-  northHand: any;
-  southHand: any;
-  eastHand: any;
-  westHand: any;
-  http: any;
-  room: any;
+  
   constructor(private router: Router) 
   {
-    this.room = client.join('your_room_name');
-    this.room.onStateChange((state: { North: { hand: any; }; South: { hand: any; }; East: { hand: any; }; West: { hand: any; }; }) => {
-      this.northHand = state.North.hand;
-      this.southHand = state.South.hand;
-      this.eastHand = state.East.hand;
-      this.westHand = state.West.hand;
-    });
-
-    room.onMessage('your_message_type', (message: { type: string; }) => {
-      // Handle specific message types
-      // Example handling:
-      if (message.type === 'game_start') {
-        console.log('The game has started!');
-      } else if (message.type === 'game_end') {
-        console.log('The game has ended!');
-      }
-      // Add more custom message handling as needed
-    });
-    const onClick = (chosenCard: any) => {
-      // Send the chosen card to the server
-      room.send('card_chosen', { card: chosenCard });
-    };
-  
+    room.onStateChange((state) => this.draw(state));
   }
-  
+
+  draw(state)
+  {
+    if (state.contract == -1) // atout pas encore prix
+    {
+      // atout est dans state.trump
+      // les cartes du joueurs sont dans state.[North ou South ou East ou West].hands
+      // le tour est dans state.turn
+      // score dans state.teamBlue et state.teamRed
+    }
+    else // manches
+    {
+      // les cartes du joueurs sont dans state.[North ou South ou East ou West].hands
+      // cartes joues sur table sont state.chosen contenant {"North": "5D"} etc
+      // cartes choisies par le joueur state.[North ou South ou East ou West].chosenCard
+      // le tour est dans state.turn
+      // score dans state.teamBlue et state.teamRed
+      // numero de plie dans state.fold
+    }
+
+    // IMPORTANT: tu dois savoir la direction de ce joueur, soit North, South, East ou West
+    // donc cherce dans state.North, state.South etc celle qui nest pas vide
+    // quelque soit la direction il est toujours en bas dans le front end, et les autres sont dans la direction de la montre comme ca North -> East -> South -> West
+    // celle ci n'est important que pour mettre en accent a qui est le tour dans le front
+  }
+
+  handleClick(id)
+  {
+    if (room.state.contract == -1)
+    {
+      room.send("action", {take: true}); // ou bien false pour specifier si le joueur accept l atout
+    }
+    else // manche
+    {
+      room.send("action", {chosen: id}); // la carte choisie (ou clicker)
+      // moi qui gere si son tour ou non, pas de souci
+      // id="5S" par exeeple
+    }
+  }
 }
