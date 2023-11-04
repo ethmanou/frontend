ae0e57bb02afaace7b10726ca841137ffadc9da3
Nouhaila JABBAR <nouhailajabbar6@gmail.com>
2023-11-04 13:20:51 +0100

    last push

--- src/app/regles/regles.component.ts
+++ src/app/regles/regles.component.ts
@@ -1,53 +1,22 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-regles',
+  selector: 'app-video',
   templateUrl: './regles.component.html',
-  styleUrls: ['./regles.component.css'],
-  animations: [
-    trigger('hideAnimation', [
-      state('void', style({ opacity: 0 })),
-      transition(':enter', [
-        animate('0.5s', style({ transform: 'translateX(0)', opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('0.5s', style({ transform: 'translateX(100px)', opacity: 0 })),
-      ]),
-    ])
-  ]
+  styleUrls: ['./regles.component.css']
 })
-export class ReglesComponent {
-  //playerHands: PlayerHand[] = [];
-  playerId: string = "north";
-  player1Hand: string[] = []; // Déclarez les cartes de la main du joueur 1
-  player2Hand: string[] = []; // Déclarez les cartes de la main du joueur 2
-  player3Hand: string[] = []; // Déclarez les cartes de la main du joueur 3
-  player4Hand: string[] = []; // Déclarez les cartes de la main du joueur 4
-  playedCards: string[] = [];
+export class ReglesComponent implements OnInit {
 
-  constructor() {}
+  showVideo: boolean = false;
 
-  ngOnInit() {
-    this.player1Hand = ['7H', '8P', 'AP', 'QC', '10T'];
-    this.player2Hand = ['6H', '7P', '9P', 'KT', '7C'];
-    this.player3Hand = ['10H', 'KP', '8T', 'A', '9C'];
-    this.player4Hand = ['AH', '10P', '7T', '9T', '10C'];
-    //this.playedCard = '7H';
+  constructor() { }
+
+  ngOnInit(): void {
   }
 
-  selectCard(card: string, playerHand: string[]) {
-    const cardIndex = playerHand.indexOf(card);
-    if (cardIndex !== -1) {
-      // La carte a été trouvée dans la main du joueur
-      const selectedCard = playerHand.splice(cardIndex, 1)[0];
-      // Ajoutez la carte au tableau des cartes déjà jouées
-      
-      if (this.playedCards.length < 4) {
-        // Ajoutez la carte jouée au tableau des cartes jouées
-        this.playedCards.push(selectedCard);
-      }
-    }
+  playVideo() {
+    this.showVideo = true;
   }
 
 }
--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -8,18 +8,37 @@ import { client, room } from '../acccueil/acccueil.component';
   styleUrls: ['./session.component.css']
 })
 export class SessionComponent {
+  northHand: any;
+  southHand: any;
+  eastHand: any;
+  westHand: any;
+  http: any;
+  room: any;
   constructor(private router: Router) 
   {
-    room.onStateChange((state) => this.draw(state));
-  }
+    this.room = client.join('your_room_name');
+    this.room.onStateChange((state: { North: { hand: any; }; South: { hand: any; }; East: { hand: any; }; West: { hand: any; }; }) => {
+      this.northHand = state.North.hand;
+      this.southHand = state.South.hand;
+      this.eastHand = state.East.hand;
+      this.westHand = state.West.hand;
+    });
 
-  draw(state)
-  {
-
+    room.onMessage('your_message_type', (message: { type: string; }) => {
+      // Handle specific message types
+      // Example handling:
+      if (message.type === 'game_start') {
+        console.log('The game has started!');
+      } else if (message.type === 'game_end') {
+        console.log('The game has ended!');
+      }
+      // Add more custom message handling as needed
+    });
+    const onClick = (chosenCard: any) => {
+      // Send the chosen card to the server
+      room.send('card_chosen', { card: chosenCard });
+    };
+  
   }
-
-  onClick()
-  {
-    
-  }
+  
 }
